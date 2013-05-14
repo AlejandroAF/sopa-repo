e8a68da964bee265fb58d0b5ee03304ed1ce788c
ale <ale@ale.com>
2013-05-14 15:13:46 -0300

    elimino

--- nivel/Debug/src/nivel.d
+++
@@ -1,6 +0,0 @@
-src/nivel.d: ../src/nivel.c ../src/tad_items.h \
- /home/utnso/workspacePruebasTP/sharedNivel/src/nivel.h
-
-../src/tad_items.h:
-
-/home/utnso/workspacePruebasTP/sharedNivel/src/nivel.h:
--- nivel/Debug/src/tad_items.d
+++
@@ -1,6 +0,0 @@
-src/tad_items.d: ../src/tad_items.c ../src/tad_items.h \
- /home/utnso/workspacePruebasTP/sharedNivel/src/nivel.h
-
-../src/tad_items.h:
-
-/home/utnso/workspacePruebasTP/sharedNivel/src/nivel.h:
